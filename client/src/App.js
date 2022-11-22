13ac295b14092880c8f93acd0bc362c75e3f2bce
nitishpratap18 <nitish@itinker.io>
2022-11-22 22:42:44 +0530

    added SignUp page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
 import {SignIn} from "./Components/SignIn/SignIn";
+import {SignUp} from "./Components/SignUP/SignUp"
 function App() {
   return (
     <div className="App">
-        <SignIn></SignIn>
+        <SignUp></SignUp>
     </div>
   );
 }
---
+++ client/src/Components/SignUP/SignUp.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import style from './style.css'
+import logo from './logo.png'
+
+export function SignUp() {
+    return (
+        <>
+            <div className={"parent"}>
+                <div className={"color"}>
+
+                </div>
+                <div className={"non-color"}>
+                    
+                </div>
+            </div>
+        </>
+    )
+}
