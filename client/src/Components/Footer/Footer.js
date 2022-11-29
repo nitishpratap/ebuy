4b37ad3a2b4142db49a1fd06eff1e6ac3bdfb5ce
nitishpratap18 <nitish@itinker.io>
2022-11-29 23:35:48 +0530

    footer UI done

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -4,10 +4,21 @@ import service1 from "./service1.png"
 import service2 from "./service2.png"
 import service3 from "./service3.png"
 import service4 from "./service4.png"
+import qr from "./qr.png"
+import playStore from "./play-store.png"
+import appStore from "./app-store.png"
 import "./style.css"
 import {AiOutlineMail} from "react-icons/ai"
 import {BsPhone} from "react-icons/bs"
-
+import {GrFacebookOption} from "react-icons/gr"
+import {AiOutlineInstagram} from "react-icons/ai"
+import {AiOutlineTwitter} from "react-icons/ai"
+import {FaLinkedinIn} from "react-icons/fa"
+import {FaPaypal, FaCcMastercard, FaCcVisa} from "react-icons/fa"
+// import {FaCcMastercard} from "react-icons/fa"
+// import {FaCcVisa} from "react-icons/fa"
+const socialMedia = [GrFacebookOption, AiOutlineInstagram, AiOutlineTwitter, FaLinkedinIn]
+const payments = [FaPaypal, FaCcMastercard, FaCcVisa]
 const ColoredLine = ({color}) => (
     <hr
         style={{
@@ -58,13 +69,43 @@ const QuickLinks = () =>
         </div>
     )
 
-const DownloadApp = ()=>(
+const DownloadApp = () => (
     <div>
         <h3>Download App</h3>
         <p>Save $3 With App & New User only</p>
+        <div className={"download-logo"}>
+            <img src={qr}/>
+            <div>
+                <a href={""}><img src={playStore}/></a>
+                <br/><br/>
+                <a href={""}><img src={appStore}/></a>
+            </div>
+        </div>
     </div>
+)
 
+const SocialLinks = () => (
+    <div>
+        {socialMedia.map((Icon, i) => (
+            <>
+                <a href={""}><Icon className = "icon icon-wrap"/></a>
+            </>
 
+        ))}
+    </div>
+)
+
+const RightText = () => (
+    <p>© 2022. All rights reserved by Mahima & Nitish.</p>
+)
+
+const Accept = () => (
+    <div className = "accept">
+        <p>Accept For </p>
+        {payments.map((Icon, i) => (
+            <Icon className = "icon accept-icon "/>
+        ))}
+    </div>
 )
 
 
@@ -79,14 +120,19 @@ export function Footer() {
                     <FirstView title="24 Hour Return Policy" lowTitle="No question ask." image={service3}/>
                     <FirstView title="Pro Quality Support" lowTitle="24/7 Live support." image={service4}/>
                 </div>
-                <ColoredLine />
+                <ColoredLine/>
                 <div className={"headers"}>
                     <Support/>
                     <Account/>
                     <QuickLinks/>
                     <DownloadApp/>
                 </div>
-                <ColoredLine />
+                <ColoredLine/>
+                <div className={"bottom"}>
+                        <SocialLinks></SocialLinks>
+                        <RightText></RightText>
+                        <Accept ></Accept>
+                </div>
             </div>
         </>
     )
