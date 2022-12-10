cc251480d612e418c1cfd46698ab7665c20a241d
nitishpratap18 <nitish@itinker.io>
2022-12-10 17:11:37 +0530

    updated Home and trying to make navbar

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -4,13 +4,24 @@ import {SignUp} from "../SignUP/SignUp";
 import {SignIn} from "../SignIn/SignIn";
 import {Route} from "react-router-dom";
 import {Footer} from "../Footer/Footer";
-import{Error} from "../404/Error"
-import{Coming} from "../comingSoon/Coming"
+import {Error} from "../404/Error"
+import {Coming} from "../comingSoon/Coming"
+import logo from "./logo.png"
 
 export function Home() {
     return (
         <>
-            <h1>Hello home</h1>
+            <div className={"start"}>
+                <div className={"navbar"}>
+                    <img className={"home-logo"} src={logo} style={{width: "150px", height: "50px"}}/>
+                    <div className={"navbar-links"}>
+                        <a className={"navbar-link"}>Home</a>
+                        <a className={"navbar-link"}>shop</a>
+                        <a className={"navbar-link"}>about</a>
+                        <a className={"navbar-link"}>contact</a>
+                    </div>
+                </div>
+            </div>
             <div style={{textAlign: "center"}}>
                 <a href={"/login"} style={{textDecoration: "none"}}>login</a>
                 <br/>
