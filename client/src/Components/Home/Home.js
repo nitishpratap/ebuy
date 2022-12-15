da7e7158e263fa94c305f960c5faa6e1ac92493a
nitishpratap18 <nitish@itinker.io>
2022-12-16 01:07:24 +0530

    modified the navbar of home

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -6,64 +6,80 @@ import {Route} from "react-router-dom";
 import {Footer} from "../Footer/Footer";
 import {Error} from "../404/Error"
 import {Coming} from "../comingSoon/Coming"
-import {CiSearch} from "react-icons/ci"
-import {CiHeart} from "react-icons/ci";
-import {CiShoppingCart} from "react-icons/ci";
+import {CiSearch, CiHeart, CiShoppingCart} from "react-icons/ci"
 import {BsPerson} from "react-icons/bs"
-
 import logo from "./logo.png"
-import {icons} from "react-icons";
+import {AiFillFire} from "react-icons/ai"
+import {starArray} from "../Utils/constant"
+
 
 export const Navbar = () => {
     return (
         <>
             <div className={"start"}>
+
                 <div className={"navbar"}>
                     <img className={"home-logo"} src={logo} style={{width: "150px", height: "50px"}}/>
                     <div className={"navbar-links"}>
-                        <a className={"navbar-link"}><h5>Home</h5></a>
-                        <a className={"navbar-link"}><h5>Shop</h5></a>
-                        <a className={"navbar-link"}><h5>About</h5></a>
-                        <a className={"navbar-link"}><h5>Contact</h5></a>
+                        <a className={"navbar-link"} href={"/"}><h5>Home</h5></a>
+                        <a className={"navbar-link"} href={"/signup"}><h5>Signup</h5></a>
+                        <a className={"navbar-link"} href={"/coming"}><h5>Coming</h5></a>
+                        <a className={"navbar-link"}href= {"/wishlist"}><h5>Wishlist</h5></a>
                     </div>
                     <NavbarIcons/>
                 </div>
+                <BackgroundText productName="Nitish Pratap Yadav"/>
             </div>
         </>
     )
 }
 
-export const NavbarIcons = ()=>{
-    return(
+const BackgroundText = (props) => {
+    return (
+        <div className={"background-text"}>
+            <div className={"deals"}>
+            <p style={{color : "red"}}><AiFillFire/> Hot deals In This Week</p>
+            <h1>{props.productName}</h1>
+            <div className={"flex buttons-star"}>
+                <button>{CiShoppingCart} Shop Now</button>
+                <div className="topbar-links inline-flex">
+                    <ul style={{display: "flex"}}>
+                        {starArray.map((Icon, i) => (
+                            <li key={i} style={{listStyle: "none"}}>
+                                <Icon/>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            </div>
+
+            <div className={"bigger-image home-bg-image"} >
+                <img />
+            </div>
+            <div className={"shape2 home-bg-image"}>
+
+            </div>
+
+        </div>
+    )
+}
+
+export const NavbarIcons = () => {
+    return (
         <div className={"nav-icons"}>
             <CiSearch className={"nav-icon"}/>
             <CiHeart className={"nav-icon"}/>
             <CiShoppingCart className={"nav-icon"}/>
             <BsPerson className={"nav-icon"}/>
         </div>
-        )
+    )
 }
 
 export function Home() {
     return (
         <>
             <Navbar/>
-            <div style={{textAlign: "center"}}>
-                <a href={"/login"} style={{textDecoration: "none"}}>login</a>
-                <br/>
-                <br/>
-                <a href={"/signup"} style={{textDecoration: "none"}}>signup</a>
-                <br/>
-                <br/>
-                <a href={"/error"} style={{textDecoration: "none"}}>Error</a>
-                <br/>
-                <br/>
-
-                <a href={"/coming"} style={{textDecoration: "none"}}>coming</a>
-                <br/>
-                <br/>
-                <a href={"/wishlist"} style={{textDecoration: "none"}}>Wishlist</a>
-            </div>
             <Footer></Footer>
         </>
     )
---
+++ client/src/Components/Utils/constant.js
@@ -0,0 +1,5 @@
+import {AiOutlineStar,AiTwotoneStar} from "react-icons/ai"
+
+
+export let   starArray = [AiOutlineStar,AiOutlineStar,AiOutlineStar,AiOutlineStar,AiTwotoneStar];
+
